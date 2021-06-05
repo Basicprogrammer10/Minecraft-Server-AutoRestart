c4d11847c7475747958acb3fa5d55a7f97558e3b
Connor Slade <turtleslade@gmail.com>
2021-06-05 16:34:12 -0400

    🔮 Add new file for common functions

---
+++ src/common.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+COLOR_CODES = {'black': '30', 'red': '31', 'yellow': '33', 'green': '32', 'blue': '34',
+              'cyan': '36', 'magenta': '35', 'white': '37', 'gray': '90', 'reset': '0'}
+
+def getLastOfArray(array):
+    """Return the value of the last index of an array"""
+    return array[len(array)-1]
+
+def colored(text, color):
+    """Add the ansi color codes to text to give it color when printed"""
+    return '\033[' + COLOR_CODES[str(color).lower()] + 'm' + str(text) + "\033[0m"
+
+def debugPrint(Category, Text, Color):
+    """Format a print with the current time, a category and colored text"""
+    print(f"{colored('['+datetime.now().strftime('%H:%M:%S')+']', 'yellow')} {colored('['+Category+']', 'magenta')} {colored(Text, Color)}")
+
+__all__ = ['getLastOfArray', 'colored', 'debugPrint']
--- src/index.py
+++ src/index.py
@@ -1,50 +1,38 @@
 from subprocess import Popen, PIPE, STDOUT
-from datetime import datetime
 import os
-import re
+
+import common
 
 ########### CONFIG ###########
 toRun = ["java", "-Xmx1G", "-jar", "server.jar", "--nogui"]
 
-############ VARS ############
-DEBUG = True
-COLOR = True
-
-ColorCodes = {'black': '30', 'red': '31', 'yellow': '33', 'green': '32', 'blue': '34',
-              'cyan': '36', 'magenta': '35', 'white': '37', 'gray': '90', 'reset': '0'}
 
 ######### FUNCTIONS #########
-def colored(text, color):
-    if not COLOR: return text
-    return '\033[' + ColorCodes[str(color).lower()] + 'm' + str(text) + "\033[0m"
-
-
-def DebugPrint(Category, Text, Color):
-    if not DEBUG: return
-    print(f"{colored('['+datetime.now().strftime('%H:%M:%S')+']', 'yellow')} {colored('['+Category+']', 'magenta')} {colored(Text, Color)}")
-
-
-def getLastOfArray(array):
-    return array[len(array)-1]
 
 
 ####### MAIN FUNCTION #######
 def main():
-    process = Popen(toRun, stdout = PIPE, stderr = STDOUT)
+    process = Popen(toRun, stdout = PIPE, stderr = STDOUT) # Open a pipe to the minecraft server
+
+    # Read and print the servers Std Out
     while True:
         line = process.stdout.readline()
         text = line.decode("utf-8").replace('\n', '')
-        text = getLastOfArray(text.split('] '))
-        DebugPrint('Server', text, 'magenta')
+        text = common.getLastOfArray(text.split('] '))
+        common.debugPrint('Server', text, 'magenta')
         if not line: break
+
+    # Get ExitCode when server stops
     exit_code = process.wait()
-    DebugPrint('Main', 'Server Closed' if exit_code == 0 else 'Server Crashed', 'red')
+    common.debugPrint('Main', 'Server Closed' if exit_code == 0 else 'Server Crashed', 'red')
+
+    # If exit code is Non 0 then start the server again
     if exit_code != 0:
-        DebugPrint('Main', 'Trying to Restart Server', 'blue')
+        common.debugPrint('Main', 'Trying to Restart Server', 'blue')
         main()
 
 if __name__ == "__main__":
     os.chdir('server')
-    DebugPrint('Main', 'Starting...', 'green')
+    common.debugPrint('Main', 'Starting...', 'green')
     try: main()
-    except: DebugPrint('Main', 'Exiting...', 'red')
+    except: common.debugPrint('Main', 'Exiting...', 'red')
