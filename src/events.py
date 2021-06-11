f7c4eedfed1dd1978e6f10b28a1754b79e0695dc
Connor Slade <connor@connorcode.com>
2021-06-11 13:33:35 -0400

    🧹 Clean up Default Events

--- src/events.py
+++ src/events.py
@@ -1,12 +1,9 @@
 import common
 
+# Basic Events
 class event():
-    def __init__(self, webhook, rawData = None):
+    def __init__(self, webhook):
         self.webhook = webhook
-        self.rawData = rawData
-
-    def serverStart(self) -> '\[.*\]: Done \(.*\)!':
-        self.webhook.send(':star2: Server Started!')
     
     def serverStop(self):
         self.webhook.send(':stop_button: Server Stoped...')
@@ -14,22 +11,32 @@ class event():
     def serverCrash(self):
         self.webhook.send(':fire: Server Crash :/ - Attempting Restart')
 
-    def chatMessage(self) -> '\[.*\]: <.*> .*':
-        sender = self.rawData.split('<')[1].split('>')[0]
-        message = self.rawData.split('> ')[1]
+# Events with stdOut Regexes
+class plugin():
+    def __init__(self):
+        self.name = 'Built In Events'
+        self.version = '1.0'
+        self.doRun = True
+
+    def serverStart(self, webhook, rawData) -> '\[.*\]: Done \(.*\)!':
+        webhook.send(':star2: Server Started!')
+
+    def chatMessage(self, webhook, rawData) -> '\[.*\]: <.*> .*':
+        sender = rawData.split('<')[1].split('>')[0]
+        message = rawData.split('> ')[1]
         message = common.makeRealNewLine(message)
-        self.webhook.send(f':speech_left: **{sender}** » {message}')
+        webhook.send(f':speech_left: **{sender}** » {message}')
 
-    def advancement(self) -> '.* has (made|completed) the (advancement|challenge) \[.*\]':
-        user = self.rawData.split(': ')[1].split(' ')[0]
-        adv = common.getLastOfArray(self.rawData.split('[')).split(']')[0]
-        thing = 'advancement' if 'advancement' in self.rawData else 'challenge'
-        self.webhook.send(f':dvd: **{user}** has completed the {thing} **{adv}**')
+    def advancement(self, webhook, rawData) -> '.* has (made|completed) the (advancement|challenge) \[.*\]':
+        user = rawData.split(': ')[1].split(' ')[0]
+        adv = common.getLastOfArray(rawData.split('[')).split(']')[0]
+        thing = 'advancement' if 'advancement' in rawData else 'challenge'
+        webhook.send(f':dvd: **{user}** has completed the {thing} **{adv}**')
 
-    def joinGame(self):
-        user = self.rawData.split(': ')[1].split(' joined the game')[0]
-        self.webhook.send(f':white_check_mark: **{user}** joined the game')
+    def joinGame(self, webhook, rawData) -> '\[.*\]: .* joined the game':
+        user = rawData.split(': ')[1].split(' joined the game')[0]
+        webhook.send(f':white_check_mark: **{user}** joined the game')
 
-    def leaveGame(self):
-        user = self.rawData.split(': ')[1].split(' left the game')[0]
-        self.webhook.send(f':x: **{user}** left the game')
+    def leaveGame(self, webhook, rawData) -> '\[.*\]: .* left the game':
+        user = rawData.split(': ')[1].split(' left the game')[0]
+        webhook.send(f':x: **{user}** left the game')
